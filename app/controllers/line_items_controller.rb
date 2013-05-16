ba509a33ab76463fa663e03c1d4a67e0c1ae1698
free <747549945@qq.com>
2013-05-16 16:16:43 +0800

    SmarterCart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -7,9 +7,8 @@ class LineItemsController < ApplicationController
 	def create
 		@cart = current_cart
 		product = Product.find(params[:product_id])
-		@line_item = @cart.line_items.build(product: product)
+		@line_item = @cart.add_product(product.id)
 		if @line_item.save
-			flash[:notice] = 'Line item was successfully create.'
 			redirect_to @line_item.cart
 		else
 			render 'new'
