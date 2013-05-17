2ad690ebdae81147f13828a90c4f73ea090f056a
free <747549945@qq.com>
2013-05-17 16:04:29 +0800

    Atom

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
   has_many :line_items
+  has_many :orders, through: :line_items
 
   default_scope order: 'title'
 
